e4a8cdd301b4c5dbfeabdab6c83b352689986a3d
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-12-24 20:58:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is a supposed to be a very long blog post blah blah blah.......";
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+            Console.ReadLine();
+        }
+    }
+}
