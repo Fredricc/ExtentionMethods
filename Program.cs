ca5a75651db360589dfc283c87de2922a45aaff1
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-12-27 09:46:52 +0300

    Extention methods from LINQ

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ namespace ExtentionMethods
             string post = "This is a supposed to be a very long blog post blah blah blah.......";
             var shortenedPost = post.Shorten(5);
 
+            IEnumerable<int> numbers = new List<int>() { 2, 4, 7, 34, 78};
+            var max = numbers.Max();
+
+            Console.WriteLine(max);
             Console.WriteLine(shortenedPost);
             Console.ReadLine();
         }
